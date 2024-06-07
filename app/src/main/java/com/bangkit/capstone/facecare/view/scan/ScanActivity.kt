f73978f1ee9f0c9b1cd55f3d4d0aa6599ff21607
Syarif Hidayatulloh <iamsyarifh@gmail.com>
2024-06-07 21:53:07 +0700

    Add features of adding image, and fix the result UI

--- app/src/main/java/com/bangkit/capstone/facecare/view/scan/ScanActivity.kt
+++ app/src/main/java/com/bangkit/capstone/facecare/view/scan/ScanActivity.kt
@@ -1,21 +1,102 @@
 package com.bangkit.capstone.facecare.view.scan
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.capstone.facecare.R
+import com.bangkit.capstone.facecare.databinding.ActivityScanBinding
+import com.bangkit.capstone.facecare.databinding.ActivitySignupBinding
+import com.bangkit.capstone.facecare.view.login.LoginActivity
+import com.bangkit.capstone.facecare.view.result.ResultActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ScanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScanBinding
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
+    private var selectedImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityScanBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_scan)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val imageBitmap = result.data?.extras?.get("data") as Bitmap
+                binding.imageButton.setImageBitmap(imageBitmap)
+                binding.imageButton.setBackgroundResource(R.drawable.bg_add_image_transparent)
+            }
+        }
+
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                this.selectedImageUri = result.data?.data
+                binding.imageButton.setImageURI(selectedImageUri)
+                binding.imageButton.setBackgroundResource(R.drawable.bg_add_image_transparent)
+            }
+        }
+
+        binding.imageButton.setOnClickListener {
+            showBottomSheetDialog()
+        }
+
+        binding.scanBtn.setOnClickListener {
+            scanNow()
+        }
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        val tvCamera = bottomSheetView.findViewById<TextView>(R.id.tvCamera)
+        val tvGallery = bottomSheetView.findViewById<TextView>(R.id.tvGallery)
+
+        tvCamera.setOnClickListener {
+            openCamera()
+            bottomSheetDialog.dismiss()
+        }
+
+        tvGallery.setOnClickListener {
+            openGallery()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.show()
+    }
+
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            cameraLauncher.launch(takePictureIntent)
+        }
+    }
+
+    private fun openGallery() {
+        val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(pickPhotoIntent)
+    }
+
+    private fun scanNow(){
+        startActivity(Intent(this, ResultActivity::class.java))
     }
 }
